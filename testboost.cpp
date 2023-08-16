3617b24cbbde613b7fe071f16edaa3f94b4622f8
DESKTOP-FJNRMGB\door7 <roynoan@gmail.com>
2023-08-16 13:39:09 +0200

    udpate

--- testboost.cpp
+++ testboost.cpp
@@ -1,14 +1,5 @@
+#include <boost/algorithm/string/classification.hpp>
 
-#include <boost/log/trivial.hpp>
-
-int main()
-{
-    BOOST_LOG_TRIVIAL(trace) << "A trace severity message";
-    BOOST_LOG_TRIVIAL(debug) << "A debug severity message";
-    BOOST_LOG_TRIVIAL(info) << "An informational severity message";
-    BOOST_LOG_TRIVIAL(warning) << "A warning severity message";
-    BOOST_LOG_TRIVIAL(error) << "An error severity message";
-    BOOST_LOG_TRIVIAL(fatal) << "A fatal severity message";
-
+int main() {
     return 0;
 }
